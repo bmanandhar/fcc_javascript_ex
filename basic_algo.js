ccb8206d058cb8e8ba988ddf329558e839a021b8
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-25 11:48:48 -0800

    #232 and up

--- basic_algo.js
+++ basic_algo.js
@@ -1,7 +1,15 @@
-//#232
+//#231
 function reverseString(str) {
   var strReverse = str.split('').reverse().join('');
   return strReverse;
 }
-
 reverseString("hello");
+//#232
+function factorialize(num) {
+  var factorial = 1;
+  for (var n = 2; n <= num; n ++)
+    factorial *= n;
+  return factorial;
+}
+
+factorialize(5);
