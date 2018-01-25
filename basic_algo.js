f5286e7056efdf0d651038472865f754fa02fb85
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-25 14:20:38 -0800

    #234 and up

--- basic_algo.js
+++ basic_algo.js
@@ -21,3 +21,22 @@ function palindrome(str) {
   return normalizedStr === reverseStr;
 }
 palindrome("My age is 0, 0 si ega ym.");
+//Longest length strinh
+
+function findLongestWord(str) {
+
+  var splitWord = [];
+  var longest = 0;
+
+  splitWord = str.split(" ");
+
+  for (i = 0; i < splitWord.length; i++) {
+    if (splitWord[i].length > longest) {
+      longest = splitWord[i].length;
+    }
+  }
+
+  return longest;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
