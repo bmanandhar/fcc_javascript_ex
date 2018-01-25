60b3d69ebb7d98a0c67830bc1e4acceee66ecbc1
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-25 13:45:26 -0800

    #232 and up

--- basic_algo.js
+++ basic_algo.js
@@ -13,3 +13,11 @@ function factorialize(num) {
 }
 
 factorialize(5);
+// Palindrome
+
+function palindrome(str) {
+  var normalizedStr = str.replace(/[\W_]/g, '').toLowerCase();
+  var reverseStr = normalizedStr.split('').reverse().join('');
+  return normalizedStr === reverseStr;
+}
+palindrome("My age is 0, 0 si ega ym.");
